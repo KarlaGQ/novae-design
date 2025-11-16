83b3b4a6689eb79b719470a7025d377b998b43bd
Karla Giselle Quintanar Olguin <kgq7112@rit.edu>
2025-11-15 21:37:37 -0500

    added java

---
+++ Java.js
@@ -0,0 +1,19 @@
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href"))
+      .scrollIntoView({ behavior: "smooth" });
+  });
+});
+
+const elements = document.querySelectorAll(".fade");
+
+window.addEventListener("scroll", () => {
+  elements.forEach(el => {
+    if (el.getBoundingClientRect().top < window.innerHeight - 50) {
+      el.classList.add("show");
+    }
+  });
+});
+
+document.getElementById("year").textContent = new Date().getFullYear();
